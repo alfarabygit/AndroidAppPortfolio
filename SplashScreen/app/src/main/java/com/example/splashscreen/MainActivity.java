5d3596c4a71d60cf35bf351c95d4d2adb186c94b
alfarabygit <alfarabyindra@gmail.com>
2023-03-27 11:29:29 +0700

    upload projects Android Studio Portfolio

---
+++ SplashScreen/app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private int loading=5000; //5000 = 5 detik
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(MainActivity.this, Home.class);
+                startActivity(home);
+                finish();
+            }
+        },loading);
+    }
+}
